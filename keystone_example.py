e4bf1b55a1ff3028cd5e370eea5514bced8ae88b
Lars Kellogg-Stedman <lars@redhat.com>
2014-09-29 19:56:14 -0400

    take a param list instead of args

--- keystone_example.py
+++ keystone_example.py
@@ -9,7 +9,12 @@ import keystoneclient.v2_0.client as ksclient
 # Import code common to all of the examples.
 import common
 
-def get_keystone_client(args):
+def get_keystone_client(os_username=None,
+                        os_password=None,
+                        os_tenant_name=None,
+                        os_tenant_id=None,
+                        os_auth_url=None
+                        ):
     '''Returns a keystone client.  Other examples that need an
     authenticated keystone client can simply:
 
@@ -20,11 +25,11 @@ def get_keystone_client(args):
         client = keystone_example.get_keystone_client(args)
     '''
 
-    return ksclient.Client(username=args.os_username, 
-                           password=args.os_password,
-                           tenant_name=args.os_tenant_name,
-                           tenant_id=args.os_tenant_id,
-                           auth_url=args.os_auth_url)
+    return ksclient.Client(username=os_username, 
+                           password=os_password,
+                           tenant_name=os_tenant_name,
+                           tenant_id=os_tenant_id,
+                           auth_url=os_auth_url)
      
 
 
@@ -34,7 +39,13 @@ def parse_args():
 
 def main():
     args = parse_args()
-    client = get_keystone_client(args)
+    client = get_keystone_client(
+        os_username=args.os_username,
+        os_password=args.os_password,
+        os_tenant_name=args.os_tenant_name,
+        os_tenant_id=args.os_tenant_id,
+        os_auth_url=args.os_auth_url
+    )
 
     # Iterate through the service catalog and display all endpoint
     # URLs for all services.
