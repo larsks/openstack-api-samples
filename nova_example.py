9cd52ac8deb83dce6f34c6c00d7d1e7df999439f
Lars Kellogg-Stedman <lars@redhat.com>
2014-09-29 20:21:42 -0400

    added a get_nova_client function for easier re-use

--- nova_example.py
+++ nova_example.py
@@ -10,28 +10,40 @@ from novaclient.v1_1 import client as novaclient
 import common
 import keystone_example
 
+def get_nova_client(keystone_client):
+    # We pass username, password, and project_id as None
+    # because we want to use our existing Keystone token rather
+    # than having Nova acquire a new one.
+    return novaclient.Client(
+        None,
+        None,
+        None,
+        auth_url=keystone_client.auth_url,
+        tenant_id=keystone_client.tenant_id,
+        auth_token=keystone_client.auth_token)
+
+
 def parse_args():
     p = common.create_parser()
+    p.add_argument('--all-tenants',
+                   action='store_true')
     return p.parse_args()
 
 def main():
     args = parse_args()
 
-    kc = keystone_example.get_keystone_client(args)
-
-    # We pass username, password, and project_id as None
-    # because we want to use our existing Keystone token rather
-    # than having Nova acquire a new one.
-    nc = novaclient.Client(
-        None,
-        None,
-        None,
-        auth_url=args.os_auth_url,
-        tenant_id=kc.tenant_id,
-        auth_token=kc.auth_token)
+    kc = keystone_example.get_keystone_client(
+        os_username=args.os_username,
+        os_password=args.os_password,
+        os_tenant_name=args.os_tenant_name,
+        os_tenant_id=args.os_tenant_id,
+        os_auth_url=args.os_auth_url
+    )
+    nc = get_nova_client(kc)
 
     # Print a list of running servers.
-    for server in nc.servers.list():
+    for server in nc.servers.list(search_opts={'all_tenants':
+                                               args.all_tenants}):
         print server.id, server.name
         for network_name, network in server.networks.items():
             print '    ', network_name, ', '.join(network)
