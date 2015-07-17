444fa1df5b2eb8bbbbe5bb9484c8adc03954bda2
Lars Kellogg-Stedman <lars@redhat.com>
2015-07-17 09:28:53 -0400

    put everyting in a class

--- allinone.py
+++ allinone.py
@@ -11,6 +11,7 @@ import keystoneclient.client as keystone_client
 import novaclient.client as nova_client
 import glanceclient.client as glance_client
 import cinderclient.client as cinder_client
+import neutronclient.neutron.client as neutron_client
 
 LOG = logging.getLogger(__name__)
 
@@ -19,91 +20,127 @@ logging.basicConfig(level='INFO')
 IDENTITY_API_VERSION = os.environ.get(
     'OS_IDENTITY_API_VERSION', '3')
 
+class OpenStack(object):
+    def __init__(self,
+                 sess=None,
+                 identity_api_version=None,
+                 auth_url=None):
 
-def get_keystone_v2_auth():
-    return keystone_identity.v2.Password(
-        auth_url=os.environ['OS_AUTH_URL'],
-        username=os.environ['OS_USERNAME'],
-        password=os.environ['OS_PASSWORD'],
-        tenant_name=os.environ['OS_TENANT_NAME'])
+        self.auth_url = (
+            auth_url if auth_url
+            else os.environ.get('OS_AUTH_URL'))
+        self.identity_api_version = identity_api_version
+        self.sess = sess or self.get_session()
 
+    def get_session(self):
+        if self.auth_url is None:
+            raise ValueError('Missing auth_url')
 
-def get_keystone_v3_auth():
-    return keystone_identity.v3.Password(
-        auth_url=os.environ['OS_AUTH_URL'],
-        username=os.environ['OS_USERNAME'],
-        password=os.environ['OS_PASSWORD'],
-        user_domain_id=os.environ.get('OS_USER_DOMAIN_ID',
-                                      'default'),
-        project_name=os.environ['OS_TENANT_NAME'],
-        project_domain_id=os.environ.get('OS_PROJECT_DOMAIN_ID',
-                                         'default'),
-    )
+        if self.identity_api_version is None:
+            self.identity_api_version = (
+                '2' if 'v2.0' in self.auth_url
+                else '3')
 
-
-def get_session(auth=None, identity_api_version=IDENTITY_API_VERSION):
-    if auth is None:
-        if identity_api_version == '3':
+        if self.identity_api_version == '3':
             LOG.info('using keystone v3 api')
-            auth = get_keystone_v3_auth()
+            auth = self.get_keystone_v3_auth()
         else:
             LOG.info('using keystone v2 api')
-            auth = get_keystone_v2_auth()
+            auth = self.get_keystone_v2_auth()
 
-    # establish a keystone session
-    sess = keystone_session.Session(auth=auth)
+        # establish a keystone session
+        sess = keystone_session.Session(auth=auth)
 
-    return sess
+        return sess
 
+    def get_keystone_v2_auth(self):
+        return keystone_identity.v2.Password(
+            auth_url=self.auth_url,
+            username=os.environ['OS_USERNAME'],
+            password=os.environ['OS_PASSWORD'],
+            tenant_name=os.environ['OS_TENANT_NAME'])
 
-def get_keystone_client(sess, identity_api_version=IDENTITY_API_VERSION):
-    kc = keystone_client.Client(identity_api_version,
-                                session=sess,
-                                project_id=sess.get_project_id(),
-                                tenant_id=sess.get_project_id(),
-                                auth_url=sess.get_endpoint(
-                                    interface=keystone_auth.AUTH_INTERFACE))
+    def get_keystone_v3_auth(self):
+        return keystone_identity.v3.Password(
+            auth_url=self.auth_url,
+            username=os.environ['OS_USERNAME'],
+            password=os.environ['OS_PASSWORD'],
+            user_domain_id=os.environ.get('OS_USER_DOMAIN_ID',
+                                          'default'),
+            project_name=os.environ['OS_TENANT_NAME'],
+            project_domain_id=os.environ.get('OS_PROJECT_DOMAIN_ID',
+                                             'default'),
+        )
 
-    kc.authenticate(token=sess.get_token())
+    def get_keystone_client(self):
+        kc = keystone_client.Client(
+            self.identity_api_version,
+            session=self.sess,
+            project_id=self.sess.get_project_id(),
+            tenant_id=self.sess.get_project_id(),
+            auth_url=self.sess.get_endpoint(
+                interface=keystone_auth.AUTH_INTERFACE))
 
-    return kc
+        kc.authenticate(token=self.sess.get_token())
 
+        return kc
 
-def get_nova_client(sess):
-    return nova_client.Client('2', session=sess)
+    def get_nova_client(self):
+        return nova_client.Client('2', session=self.sess)
 
+    def get_glance_client(self):
+        return glance_client.Client(
+            '2', endpoint=self.keystone.service_catalog.url_for(
+                service_type='image',
+                endpoint_type='publicURL'),
+            token=self.sess.get_token())
 
-def get_glance_client(sess):
-    kc = get_keystone_client(sess)
+    def get_cinder_client(self):
+        return cinder_client.Client('2', session=self.sess)
 
-    return glance_client.Client(
-        '2', endpoint=kc.service_catalog.url_for(
-            service_type='image',
-            endpoint_type='publicURL'),
-        token=sess.get_token())
+    def get_neutron_client(self):
+        return neutron_client.Client('2.0', session=self.sess)
 
+    @property
+    def keystone(self):
+        try:
+            return self._keystone
+        except AttributeError:
+            self._keystone = self.get_keystone_client()
+            return self._keystone
 
-def get_cinder_client(sess):
-    return cinder_client.Client('2', session=sess)
+    @property
+    def nova(self):
+        try:
+            return self._nova
+        except AttributeError:
+            self._nova = self.get_nova_client()
+            return self._nova
 
+    @property
+    def glance(self):
+        try:
+            return self._glance
+        except AttributeError:
+            self._glance = self.get_glance_client()
+            return self._glance
 
-def get_all_clients(sess=None):
-    if not sess:
-        sess = get_session()
+    @property
+    def cinder(self):
+        try:
+            return self._cinder
+        except AttributeError:
+            self._cinder = self.get_cinder_client()
+            return self._cinder
 
-    kc = get_keystone_client(sess)
-    nc = get_nova_client(sess)
-    gc = get_glance_client(sess)
-    cc = get_cinder_client(sess)
+    @property
+    def neutron(self):
+        try:
+            return self._neutron
+        except AttributeError:
+            self._neutron = self.get_neutron_client()
+            return self._neutron
 
-    return {
-        'session': sess,
-        'keystone': kc,
-        'nova': nc,
-        'glance': gc,
-        'cinder': cc,
-    }
 
 if __name__ == '__main__':
-    sess = get_session()
-    clients = get_all_clients(sess)
+    clients = OpenStack()
